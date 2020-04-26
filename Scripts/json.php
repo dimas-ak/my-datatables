5425672657c73df06b7d4daff1531e550ad83ec9
Dimas Awang Kusuma <arjunane.kajen@gmail.com>
2020-04-26 21:17:45 +0700

    modify json file

--- Scripts/json.php
+++ Scripts/json.php
@@ -1,10 +1,11 @@
 <?PHP
 $json = [];
-$grade = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+$_grade = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 $place  = ["Pekalongan", "Kajen", "Bojong", "Wiradesa", "Kedungwuni", "Linggo Asri"];
 for($i = 1; $i <= 100; $i++)
 {
-    $random_grade = array_rand($grade, 1);
+    $random_grade = array_rand($_grade, 1);
+    $grade        = $_grade[$random_grade];
     $random_place = array_rand($place, 1);
     $gender = ($i % 2 == 0) ? "Male" : "Female";
     $date   = date("Y-m-d", strtotime("-$i day"));
@@ -12,7 +13,8 @@ for($i = 1; $i <= 100; $i++)
     [
         $i,
         'Name number ' . $i,
-        $grade[$random_grade],
+        $grade,
+        $grade >= 70 ? 1 : 0,
         $gender,
         $date,
         $place[$random_place]
